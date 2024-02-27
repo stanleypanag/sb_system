983c08ef2814f888d42247ff358bcdee0a129088
Stanley Panag <130243031+stanleypanag@users.noreply.github.com>
2024-02-28 03:33:25 +0800

    adding json sample to resolution

--- client/src/components/c_guest/resolution/Resolution.jsx
+++ client/src/components/c_guest/resolution/Resolution.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./resolution.css";
+import {resolutionData} from "./resolution.js";
 
 const Resolution = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <>
-      <main>
+      <main className="h-full">
         <div className="resolution-content mt-20">
           <div className="tracking-widest text-center text-gray-700 text-5xl">
             <h1>Naic Resolutions</h1>
@@ -15,6 +18,7 @@ const Resolution = () => {
           </label>
           <div className="flex justify-center mt-10 rounded-lg shadow-sm">
             <input
+              onChange={(e) => setSearch(e.target.value)}
               type="text"
               id="hs-trailing-button-add-on-with-icon"
               name="hs-trailing-button-add-on-with-icon"
@@ -44,42 +48,61 @@ const Resolution = () => {
 
           <div className="max-w-[40rem] px-10 py-10 sm:px-6 lg:px-8 lg:py-16 mx-auto">
             {/* <!-- Grid --> */}
+
+            {/* <!--Resolution Card --> */}
             <div className="grid lg:grid-cols-1 lg:gap-y-10 gap-10">
-              {/* <!--Resolution Card --> */}
-              <div className="flex flex-col bg-white border border-gray-400 shadow-sm">
-                <div className="bg-gray-800 border-b py-3 px-4 md:py-4 md:px-5">
-                  <p className="mt-1 text-sm text-white">Resolution No. 1</p>
-                </div>
-                <div className="p-4 md:p-5">
-                  <h3 className="text-lg font-bold text-gray-800">
-                    Card title
-                  </h3>
-                  <p className="mt-2 text-gray-500">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
-                  <a
-                    className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none"
-                    href="#"
+              {resolutionData
+                .filter((item) => {
+                  const searchLower = search.toLowerCase();
+                  const yearString = String(item.year); // Convert to string
+                  return (
+                    searchLower === "" ||
+                    item.resolution_number
+                      .toLowerCase()
+                      .includes(searchLower) ||
+                    item.title.toLowerCase().includes(searchLower) ||
+                    yearString.toLowerCase().includes(searchLower)
+                  );
+                })
+                .map((item) => (
+                  <div
+                    className="flex flex-col bg-white border border-gray-400 shadow-sm"
+                    key={item.id}
                   >
-                    Card link
-                    <svg
-                      className="flex-shrink-0 w-4 h-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="m9 18 6-6-6-6" />
-                    </svg>
-                  </a>
-                </div>
-              </div>
+                    <div className="bg-gray-800 border-b py-3 px-4 md:py-4 md:px-5">
+                      <p className="mt-1 text-sm text-white">
+                        Resolution No. {item.resolution_number}
+                      </p>
+                    </div>
+                    <div className="p-4 md:p-5">
+                      <h3 className="text-lg font-bold text-gray-800">
+                        {item.title}
+                      </h3>
+                      <p className="mt-2 text-gray-500">{item.content}</p>
+                      <a
+                        className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-gray-600 hover:text-gray-800 disabled:opacity-50 disabled:pointer-events-none"
+                        href="#"
+                      >
+                        Card link
+                        <svg
+                          className="flex-shrink-0 w-4 h-4"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="m9 18 6-6-6-6" />
+                        </svg>
+                      </a>
+                    </div>
+                  </div>
+                ))}
+
               {/* <!--Resolution End Card --> */}
             </div>
             {/* <!-- End Grid --> */}
